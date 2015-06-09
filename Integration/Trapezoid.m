652010a6061c0313b2a632a5fb53bbbf8c6adc9e
Pablo Brubeck <pbrubeck95@gmail.com>
2015-06-08 19:46:51 -0500

    Commit?

--- Integration/Trapezoid.m
+++ Integration/Trapezoid.m
@@ -1,20 +1,11 @@
-function J = Trapezoid(f, a, b)
-% Iterative trapezoidal method.
-n=1;
-h=b-a;
-J=h*(f(a)+f(b))/2;
-err=1;
-while(err>1E-5)
-    s=0;
-    n=2*n;
-    xi=a+h/2;
-    for j=1:2:n-1
-        s=s+f(xi);
-        xi=xi+h;
-    end
-    h=h/2;
-    aux=J/2+h*s;
-    err=abs((J-aux)/aux);
-    J=aux;
+function J = Trapezoid(f, a, b, n)
+% Simple trapezoidal method.
+h=(b-a)/n;
+xi=a;
+J=(f(a)+f(b))/2;
+for j=1:1:n-1
+   J=J+f(xi);
+   xi=xi+h;
 end
+J=h*J;
 end
