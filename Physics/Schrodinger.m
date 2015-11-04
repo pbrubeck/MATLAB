94655410b2cd57546db1a6407fc27833c55b87b5
PabloBrubeck <pbrubeck95@gmail.com>
2015-11-04 10:40:32 -0600

    Quantum tunneling

--- Physics/Schrodinger.m
+++ Physics/Schrodinger.m
@@ -3,8 +3,9 @@ tic();
 [D, x]=chebD(n+2); x=(1+x)/2;
 D2=4*D^2; D2=D2(2:end-1,2:end-1);
 %V=2000./abs(x-0.5);
-V=2000*(x-0.5).^2;
+%V=2000*(x-0.5).^2;
 %V=(x<0.7).*(-500*x);
+V=400*((x<0.6)-(x<0.4));
 H=-D2+diag(V(2:end-1));
 [S, E]=eigs(H, m, 'sm'); [E, idx]=sort(diag(E)); S=S(:,idx);
 Psi1=zeros(n+2, m);
@@ -20,8 +21,9 @@ v=[1:m; E']; title(sprintf('E%i=%f ', v(:)));
 tic();
 x=linspace(0,1,n+2); x=x(:);
 %V=2000./abs(x-0.5);
-V=2000*(x-0.5).^2;
+%V=2000*(x-0.5).^2;
 %V=(x<0.7).*(-500*x);
+V=400*((x<0.6)-(x<0.4));
 M=diag((pi*(1:n)).^2)+2/(n+1)*dst(dst(diag(V(2:end-1)))')';
 [S, E]=eigs(M, m, 'sm'); [E, idx]=sort(diag(E)); S=S(:,idx);
 Psi2=zeros(n+2, m);
@@ -35,7 +37,7 @@ a=normc(1./E);
 figure(3);
 h=plot(x, zeros(size(x)));
 ylim([0, 5]);
-for t=0:0.00005:1
+for t=0:0.0005:10
     h.YData=abs(Psi2*(a.*exp(-1i*E*t))).^2;
     drawnow();
 end
