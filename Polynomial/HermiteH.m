6d5bf3ddb8dae2f1e19904c12a7ef2b1ac7c992b
Pablo Brubeck <pbrubeck95@gmail.com>
2015-12-09 19:22:34 -0600

    Hermite Fix

--- Polynomial/HermiteH.m
+++ Polynomial/HermiteH.m
@@ -1,7 +1,7 @@
 function [y]=HermiteH(a, x)
-% Evaluates 
+% Evaluates the Hermite series given by the coefficients a(n) 
 n=length(a);
-b=(n>2)*a(n); bb=zeros(size(x));
+b=(n>1)*a(n); bb=zeros(size(x));
 for k=n-2:-1:1
     temp=b;
     b=a(k+1)+2*x.*b-2*(k+1)*bb;
