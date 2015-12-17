af005793fadaedc5a6e63c6bc228abf35f74cc45
PabloBrubeck <brubeck@protonmail.com>
2015-12-17 13:20:15 -0600

    Airy plot

--- Differential Equations/Airy.m
+++ Differential Equations/Airy.m
@@ -3,13 +3,13 @@ function [u] = Airy(m, n)
 % the first m zeros
 [D, x]=chebD(n);
 D2=D^2; D2=D2(2:n-1, 2:n-1);
-[V,lambda]=eig(D2, diag(x(2:n-1)), 'vector');
-ii=find(lambda>0); 
-lambda=lambda(ii); V=V(:,ii);
-[~, ii]=sort(lambda); ii=ii(m);
+[V,lambda]=eigs(D2, diag(x(2:end-1)), m, 'sm');
+lambda=diag(lambda);
+[~, ii]=sort(lambda); ii=ii(end); 
 lambda=lambda(ii);
 u=[0; V(:, ii); 0];
 mid=(u(ceil(end/2))+u(floor(end/2+1)))/2;
 u=(airy(0)/mid)*u;
-plot(x, u); title(sprintf('lambda = %f', lambda));
+figure(1); plot(x, u);
+title(sprintf('lambda = %f', lambda));
 end
