a6e79fd0a679731ff0fe164b76f30a46cccf91b3
PabloBrubeck <brubeck@protonmail.com>
2016-01-12 23:02:09 -0600

    Bessel Series

---
+++ Special Functions/BesselS.m
@@ -0,0 +1,13 @@
+function [y] = BesselS(a, x)
+% Bessel Series: sum(a(n)*J(n,x))
+n=length(a);
+y=(n>1)*a(n); yy=zeros(size(x));
+for k=n-2:-1:1
+    temp=y;
+    y=a(k+1)+2*k*y./x-yy;
+    yy=temp;
+end
+j0=besselj(0,x);
+j1=besselj(1,x);
+y=a(1)*j0+y.*j1-yy.*j0;
+end
