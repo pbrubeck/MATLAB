6bcc2d4498f44d965d0dc52b63836038d46bb951
Pablo Brubeck <pbrubeck95@gmail.com>
2016-03-02 17:45:00 -0600

    Mathieu almost done

--- Special Functions/MathieuS.m
+++ Special Functions/MathieuS.m
@@ -1,5 +1,6 @@
 function [sem] = MathieuS(m, n, z, q)
 j=mod(m,2);
+k=(m+j)/2;
 E=q*ones(1,n-1);
 if j==0
     D=(2:2:2*n).^2;
@@ -7,9 +8,8 @@ else
     D=[1-q, (3:2:2*n-1).^2];  
 end
 [~,B]=trideigs(D,E);
-B=B(:,(m+j)/2);
-sem=zeros(size(z));
-for i=0:n-1
-    sem=sem+B(i+1)*sin((2*i+2-j)*z);
-end
+B=B(:,k);
+BB=zeros(1,2*n+1);
+BB(3-j:2:end)=B;
+sem=-imag(fft(BB,numel(z)));
 end
