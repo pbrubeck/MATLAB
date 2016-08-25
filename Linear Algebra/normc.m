42da07f8a3bee658dbe96f8c794133c550a1fc9f
pbrubeck <brubeck@protonmail.com>
2016-08-24 23:56:25 -0500

    schrodExp

--- Linear Algebra/normc.m
+++ Linear Algebra/normc.m
@@ -1,4 +1,8 @@
 function [B] = normc(A, w)
 % Normalizes columns of A with respect to weight w
-B=bsxfun(@rdivide, A, sqrt(w(:)'*(real(A).^2+imag(A).^2)));
+if(nargin==2)
+    B=bsxfun(@rdivide, A, sqrt(w(:)'*(real(A).^2+imag(A).^2)));
+else
+    B=bsxfun(@rdivide, A, sqrt(sum(real(A).^2+imag(A).^2)));
+end
 end
---
+++ Physics/schrodExp.m
@@ -0,0 +1,28 @@
+function [] = schrodExp( N )
+[D,x]=chebD(N);
+[~,w]=ClenshawCurtis(-1,1,N-1);
+
+dt=0.001;
+V0=1000;
+T=-1/2*D*D;
+V=diag(V0*x.^2);
+H=T+V;
+
+[V,L]=eig(H(2:end-1,2:end-1),'vector');
+L=[0;0;L];
+U=zeros(N,N);
+U(:,1:2)=null(H);
+U(2:end-1,3:end)=V;
+Q=U*diag(exp(-1i*dt*L))/U;
+
+sigma=0.2;
+k=1/sigma;
+Psi0=normc(exp(-(x/sigma).^2-1i*k*x), w);
+h=plot(x,abs(Psi0).^2); drawnow; ylim([0, 4]);
+for i=1:10000
+    Psi0=Q*Psi0;
+    set(h, 'YData', abs(Psi0).^2);
+    drawnow;
+end
+end
+
