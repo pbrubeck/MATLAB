d1efe8b8dddf6b80f403ce5d08f814a3440b2ebb
PabloBrubeck <brubeck@protonmail.com>
2016-10-31 08:03:20 -0600

    herm sig

--- Analysis/hermSeries.m
+++ Analysis/hermSeries.m
@@ -1,10 +1,11 @@
 function [ c ] = hermSeries(f, n)
-[x,w]=GaussHermite(n,0,1/sqrt(2));
+sig=1/sqrt(2);
+[x,w]=GaussHermite(n,0,sig);
 Psi=zeros(n);
+a=zeros(n,1); a(end)=1;
 for i=1:n
-    a=[zeros(i-1,1); 1];
-    Psi(i,:)=HermitePsi(a,x);
+    Psi(i,:)=HermitePsi(a(end-i+1:end),x);
 end
-w=w.*exp(x.^2);
+w=w.*exp(x.^2/(2*sig^2));
 c=Psi*diag(w)*f(x(:));
 end
