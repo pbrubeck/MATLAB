4430f508f6c22bcbffc81645f983fd9e5ce76b5d
PabloBrubeck <brubeck@protonmail.com>
2016-10-31 13:58:04 -0600

    nlse exact nonlinear prop

--- PDE/nlse.m
+++ PDE/nlse.m
@@ -3,14 +3,12 @@ function [] = nlse( N )
 % 1i u_t + 1/2 u_xx +|u|^2 u = 0
 t0=-3;
 tf=3;
-dt=1/(5*N);
-x0=10;
+dt=1/(N);
+x0=3;
 
 % Domain substitution x=tan(th), psi=sec(th).*u
 [D,th]=chebD(N); th=pi/2*th; D=2/pi*D;
 x=tan(th);
-global sec2;
-sec2=sec(th).^2;
 
 % Initial condition
 psi=(1-4*(1+2i*t0)./(1+4*(x.^2+t0^2)))*exp(1i*t0);
@@ -23,7 +21,7 @@ L=[0;0;L];
 U=zeros(N);
 U(:,1:2)=null(H);
 U(2:end-1,3:end)=V;
-Q=U*diag(exp(-1i*dt*L))/U;
+Q=U*diag(exp(-1i*dt/2*L))/U;
 
 figure(1);
 h=plot(x, abs(psi), 'b', 'LineWidth', 2);
@@ -35,8 +33,10 @@ nframes=N;
 udata=zeros(nframes,N);
 udata(1,:)=abs(psi);
 for i=2:nframes
-    for t=0:dt:(tf-t0)/nframes
-        u=solveRK4(Q*u,dt);
+    for t=0:dt:(tf-t0)/(nframes+1)
+        u=Q*u;
+        u=u.*exp(1i*dt*(abs(sec(th).*u).^2));
+        u=Q*u;
     end
     psi=sec(th).*u;
     udata(i,:)=psi;
@@ -49,19 +49,4 @@ id=abs(x)<=3;
 surf(x(id),linspace(t0,tf,nframes),abs(udata(:,id)).^2);
 colormap(jet(50)); colorbar(); shading interp; view(2);
 xlabel('x'); ylabel('t'); title('|\Psi|^2');
-end
-
-function ut=partialt(u)
-% nonlinear operator
-global sec2;
-ut=1i*(sec2.*abs(u).^2).*u;
-end
-
-function u=solveRK4(u, dt)
-% time-stepping by Runge Kutta 4th order.
-k1=dt*partialt(u);
-k2=dt*partialt(u+k1/2);
-k3=dt*partialt(u+k2/2);
-k4=dt*partialt(u+k3);
-u=u+(k1+2*k2+2*k3+k4)/6;
 end
