6974f4e4264b06834877064da3b57e587f8a8de6
pbrubeck <brubeck@protonmail.com>
2018-08-22 19:01:31 -0500

    collocation soliton

--- Solitons/nlse.m
+++ Solitons/nlse.m
@@ -1,6 +1,6 @@
 function [] = nlse(N, t1, t2, L, init, method)
 % NonLinear Schrodinger Equation in one dimension
-% 1i u_t + 1/2 u_xx +|u|^2 u = 0
+% 1i u_t + 1/2 u_xx + |u|^2 u = 0
 %
 % N: number of collocation points
 % t1: initial time
@@ -9,14 +9,16 @@ function [] = nlse(N, t1, t2, L, init, method)
 % init: bright, dark1, dark2, peregrine, breather
 % method: cheb, herm, fft, dst
 
+setlatex();
+
 nframes=1024;
 dt=1/1024;
 m=ceil((t2-t1)/(dt*(nframes-1)));
 dt=(t2-t1)/(m*(nframes-1));
-Amax=2;
+Amax=4;
 
 % Linear propagator, half step
-if nargin<6, method='cheb'; end;
+if nargin<6, method='cheb'; end
 switch(method)
     case 'cheb', [x,w,D,Q]=nlsecheb(N,dt/2);
     case 'herm', [x,w,D,Q]=nlseherm(N,dt/2);
@@ -46,11 +48,11 @@ P=1i/2*w'*(u.*linprop(D,conj(u))-conj(u).*linprop(D,u)); display(P);
 E=w'*(abs(linprop(D,u)).^2+abs(u).^4);                   display(E);
 
 f1=figure(1);
-h=plot(x, abs(u), 'b', 'LineWidth', 2);
+h=plot(x, abs(u), '.b', 'LineWidth', 2);
 xlim([-L,L]); ylim([0,Amax]); axis manual;
 set(gca, 'XTick', [-L,0,L]);
 set(gca, 'YTick', [0,Amax/2,Amax]);
-xlabel('x'); title('|\Psi|');
+xlabel('$x$'); title('$|\Psi|$');
 set(gca,'FontSize',18);
 drawnow;
 
@@ -86,12 +88,12 @@ figure(2);
 roi=abs(x)<=L;
 surf(x(roi),t,abs(udata(:,roi)).^2);
 colormap(jet(256)); colorbar(); shading interp; view(2);
-xlabel('x'); ylabel('t'); title('|\Psi|^2');
+xlabel('$x$'); ylabel('$t$'); title('$|\Psi|^2$');
 
 figure(3);
 imagesc(x(roi),t,angle(udata(:,roi))); set(gca, 'YDir', 'normal');
 colormap(hsv(256)); colorbar(); shading interp;
-xlabel('x'); ylabel('t'); title('arg(\Psi)');
+xlabel('$x$'); ylabel('$t$'); title('$\arg(\Psi)$');
 end
 
 function u=linprop(Q,u)
--- Solitons/nlsecheb.m
+++ Solitons/nlsecheb.m
@@ -1,22 +1,19 @@
-function [x,w,D,Q] = nlsecheb(N, dt)
+function [x,w,D,Q] = nlsecheb(n, dt)
 % NonLinear Schrodinger Equation, Chebyshev linear propagator
 % 1i u_t + 1/2 u_xx = 0
-
+rd=[1,n];
+kd=setdiff(1:n,rd);
 % Domain substitution x=tan(th), u=sec(th).*w
-[Dt,th]=chebD(N); th=pi/2*th; Dt=2/pi*Dt;
+[Dt,th,w]=legD(n); th=pi/2*th; Dt=2/pi*Dt; w=pi/2*w;
 x=tan(th);
-[~,w]=ClenshawCurtis(-pi/2,pi/2,N);
-w=(sec(th).^2).*w(:);
 
-T=-1/2*diag(cos(th).^4)*(eye(N)+Dt*Dt);
-NT=null(T);
-[V,L]=eig(T(2:end-1,2:end-1),'vector');
-L=[0;0;L];
-S=zeros(N);
-S(:,1:2)=NT(:,1:2);
-S(2:end-1,3:end)=V;
-Q=S*(diag(exp(-1i*dt*L)))/S;
+K=-1/2*diag(cos(th).^4)*(eye(n)+Dt*Dt);
+M=eye(n);
+S=eye(n);
+L=zeros(n,1);
+[S(kd,kd),L(kd)]=eig(K(kd,kd),M(kd,kd),'vector');
+Q=S*diag(exp(-1i*dt*L))/S;
 Q=Q.*(sec(th)*cos(th'));
-
 D=diag(cos(th).^2)*Dt;
+w=(sec(th).^2).*w;
 end
